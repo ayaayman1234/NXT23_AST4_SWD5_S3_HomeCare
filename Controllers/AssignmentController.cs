45628d7c5b60d7f0de08baf49588220dfe3d2bc6
Yara Ahmed <Yara.Ahmed0207@compit.aun.edu.eg>
2026-07-05 03:42:00 +0300

    Add controllers, services, and views

---
+++ Controllers/AssignmentController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NursingCarePlatform.Web.Services.Interfaces;
+using NursingCarePlatform.Web.ViewModels;
+
+namespace NursingCarePlatform.Web.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        private readonly IAssignmentService _assignmentService;
+
+        public AssignmentController(IAssignmentService assignmentService)
+        {
+            _assignmentService = assignmentService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            var model = await _assignmentService.GetAssignmentDetailsAsync(id);
+
+            if (model == null)
+                return NotFound();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new AssignmentViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(AssignmentViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var assignmentId = await _assignmentService.CreateAssignmentAsync(model);
+
+            return RedirectToAction(nameof(Details), new { id = assignmentId });
+        }
+    }
+}
---
+++ Controllers/paymentController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NursingCarePlatform.Web.Services.Interfaces;
+using NursingCarePlatform.Web.ViewModels;
+
+namespace NursingCarePlatform.Web.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly IPaymentService _paymentService;
+
+        public PaymentController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            var model = await _paymentService.GetPaymentDetailsAsync(id);
+
+            if (model == null)
+                return NotFound();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new PaymentViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(PaymentViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var paymentId = await _paymentService.CreatePaymentAsync(model);
+
+            return RedirectToAction(nameof(Details), new { id = paymentId });
+        }
+    }
+}
